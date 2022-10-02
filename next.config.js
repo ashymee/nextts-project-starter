5a448dc74dbdea6f983f6c4ace3391804a5414c4
Aswin Himawan <ashymee@gmail.com>
2022-10-02 15:04:59 +0700

    feat: enable next-pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,38 @@
-/** @type {import('next').NextConfig} */
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  runtimeCaching,
+  dest: "public",
+  register: process.env.NODE_ENV === "production",
+  disable: process.env.NODE_ENV === "development",
+  skipWaiting: true,
+});
+const { i18n } = require("./next-i18next.config");
+
+/** @type {import('next').NextConfig} nextConfig */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      "api.codiga.io",
+      "app.codiga.io",
+      "htpqxbnegiziuzrzrgbl.supabase.co",
+      "lh3.googleusercontent.com",
+      "ipgeolocation.io",
+      "media.giphy.com",
+      "res.cloudinary.com",
+    ],
+    dangerouslyAllowSVG: true,
+    // contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: false,
+  },
+  i18n,
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
